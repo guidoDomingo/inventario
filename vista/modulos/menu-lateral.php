09963a8358b6911cefde9e55d08975cb890d1c23
guido <ruizbenitezguido11@gmail.com>
2021-05-31 22:36:45 -0400

    actualizado

---
+++ vista/modulos/menu-lateral.php
@@ -0,0 +1,131 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="vista/index3.html" class="brand-link">
+      <img src="vista/img/plantilla/thn.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Thn Paraguay</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="vista/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Guido Ruiz</a>
+        </div>
+      </div>
+
+    
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          
+          <li class="nav-item">
+            <a href="inicio" class="nav-link">
+              <i class="nav-icon fa fa-home"></i>
+              <p>
+                Inicio
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="usuarios" class="nav-link">
+              <i class="nav-icon fa fa-user"></i>
+              <p>
+                Usuarios
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Equipos
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="categorias" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Categoria</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="proveedor" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Proveedor</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="equipos" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Equipo</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Planta Industrial
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+             <li class="nav-item">
+                <a href="planta" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Planta</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="sector" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Sector</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="estacion" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Estación</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Reportes
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="mensual" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Mensual</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="semanal" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Semanal</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
